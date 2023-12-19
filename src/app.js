60d83c64af07b2234f6ae8bb3776af8c5abf20a2
Jamil Madison <contact@jamilmadison.com>
2023-12-19 13:33:03 -0800

    Add cors and dotenv dependencies, update app.js and db.js

--- src/app.js
+++ src/app.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// CORS Configuration
+app.use(cors());
 
 // Express Configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 // Router
-const adminRoutes = require('./router/admin.routes');
-const productRoutes = require('./router/product.routes');
+const adminRoutes = require("./router/admin.routes");
+const productRoutes = require("./router/product.routes");
 
 // Routes
-app.use('/admin', adminRoutes);
-app.use('/products', productRoutes);
+app.use("/admin", adminRoutes);
+app.use("/products", productRoutes);
 
+// Error Handling
 app.listen(PORT, () => {
-	console.log(`Server is on Port ${PORT}`)
+	console.log(`Server is on Port ${PORT}`);
 });
--- src/models/db.js
+++ src/models/db.js
@@ -1,25 +1,30 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-	host: process.env.HOST,
-	user: process.env.USER,
-	password: process.env. PASSWORD,
-	multipleStatements: true
-}).promise();
+require("dotenv").config();
 
-let databaseQuery = "CREATE DATABASE IF NOT EXISTS ecommerce_db; USE ecommerce_db;"
+const db = mysql
+	.createConnection({
+		host: process.env.HOST,
+		user: process.env.USER,
+		password: process.env.PASSWORD,
+		multipleStatements: true,
+	})
+	.promise();
+
+let databaseQuery =
+	"CREATE DATABASE IF NOT EXISTS ecommerce_db; USE ecommerce_db;";
 
 let adminsTableQuery = `CREATE TABLE IF NOT EXISTS admins (
 	email VARCHAR(255) NOT NULL,
 	password VARCHAR(255) NOT NULL,
 	id VARCHAR(36) NOT NULL PRIMARY KEY
-)`
+)`;
 
 let productsTableQuery = `CREATE TABLE IF NOT EXISTS products (
 	productName VARCHAR(255) NOT NULL,
 	price DECIMAL(13,2) NOT NULL,
 	id VARCHAR(36) NOT NULL PRIMARY KEY
-)`
+)`;
 
 db.query(databaseQuery);
 
