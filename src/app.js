b9e45756e5c798e08dbb277e3367c3b92123e250
Jesstin Swadley <contact@jesstin.com>
2023-12-26 20:13:07 -0800

    fix cors issue

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 // Express Configuration
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
